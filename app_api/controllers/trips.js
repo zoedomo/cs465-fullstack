65732ee02f7ead689159c3069e394bb690f54f6e
Zoe Domagalski <zoeydomo@gmail.com>
2024-02-24 14:13:00 -0600

    Initial commit

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,147 @@
+const mongoose = require('mongoose');
+const Trip = mongoose.model('trips'); 
+const User = mongoose.model('users');
+
+// GET: /trips - lists all trips 
+const tripsList = async (req, res) => {
+    Trip
+        .find({})
+        .exec((err, trips) => {
+            if (!trips) {
+                return res
+                    .status(404)
+                    .json({ "message": "trips not found"});
+            } else if (err) {
+                return res
+                    .status(404)
+                    .json(trips);
+            } else {
+                return res
+                    .status(200)
+                    .json(trips);
+            }
+        });
+
+};
+//
+const tripsFindCode = async (req, res) => {
+    Trip 
+        .find({'code': req.params.tripCode })
+        .exec((err, trip) => {
+            if (!trip) {
+                return res
+                    .status(404)
+                    .json({ "message": "trips not found"});
+
+            }else if (err){
+                return res  
+                    .status(404)
+                    .json(err);
+            }else {
+                return res
+                    .status(200)
+                    .json(trip);
+            }
+        });
+};
+
+const tripsAddTrip = async (req, res) => {
+    getUser(req, res,
+        (req, res) => { 
+        Trip
+            .create({
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            },
+            (err, trip) => {
+                if (err){
+                    return res
+                        .status(400)
+                        .json(err);
+                }else {
+                    return res  
+                        .status(201)
+                        .json(trip);
+                }
+            });
+        }
+    );
+}
+
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.body);
+    getUser(req, res,
+        (req, res) => { 
+        Trip
+            .findOneAndUpdate({ 'code': req.params.tripCode }, {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }, {new: true })
+            .then (trip => {
+                if (!trip) {
+                    return res
+                        .status(404)
+                        .send({
+                            message: "Trip not found with Code " + req.params.tripcode
+                        });
+
+                }
+                res.send(trip);
+            }).catch(err => {
+                if(err.kind === 'ObjectId') {
+                    return res
+                        .status(404)
+                        .send({
+                            message: "Trip not found with code " + req.params.tripCode
+                        });
+                }
+                return res
+                    .status(500) // Server error
+                    .json(err);
+            });
+        }
+    )
+}
+
+const getUser = (req, res, callback) => {
+    if (req.payload && req.payload.email) {
+        User
+            .findOne({ email: req.payload.email })
+                .exec((err, user) => {
+                    if(!user) {
+                        return res
+                            .status(404)
+                            .json({ "message": "User not found" });
+                    } else if (err) {
+                        console.log(err);
+                        return res
+                            .status(404)
+                            .json(err);
+                    }
+                    callback(req, res, user.name);
+                });
+    } else {
+        return res
+            .status(404)
+            .json({ "message": "User not found" });
+    }
+};
+
+module.exports = {
+    tripsList,
+    tripsFindCode,
+    tripsAddTrip,
+    tripsUpdateTrip
+};
